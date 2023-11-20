c075f46cb0c7f1754599218c1eb3209ac570733b
Raju Deb Nath <rajudevit94@gmail.com>
2023-11-20 13:26:55 +0600

    done

--- src/components/UI/SignIn.jsx
+++ src/components/UI/SignIn.jsx
@@ -52,7 +52,7 @@ export default function SignIn() {
         <p className="text-[#8A94A6] pb-2">Create an account to continue!</p>
         <div className="">
           <Input
-            className="h-10 my-5 border-2 w-full max-sm:w-3/4"
+            className="h-10 my-2 border-2 w-full max-sm:w-3/4"
             placeholder="@ Your Email"
             type="email"
             value={data.email}
@@ -61,7 +61,7 @@ export default function SignIn() {
           />
 
           <Input.Password
-            className="h-10 my-5 border-2 w-full max-sm:w-3/4"
+            className="h-10 my-2 border-2 w-full max-sm:w-3/4"
             placeholder="Create Password"
             prefix={<BiSolidLockAlt className="text-[#C1C7D0] " />}
             type="password"
@@ -73,7 +73,7 @@ export default function SignIn() {
 
         <Button
           onClick={handleFormSubmit}
-          className=" bg-[#377DFF] w-full max-sm:w-3/4 h-10 text-lg text-white hover:bg-white my-4"
+          className=" bg-[#377DFF] w-full max-sm:w-3/4 h-10 text-lg text-white hover:bg-white"
         >
           Sign In
         </Button>
